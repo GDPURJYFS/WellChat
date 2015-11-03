b3a71f94b8e77b0fd86cf1d508fc45d4b2a29d23
qyvlik <qyvlik@qq.com>
2015-11-03 19:00:21 +0800

    开始重构
    先删除android文件夹

--- android/src/org/GDPURJYFS/Sparrow/QtNativeForAndroid.java
+++
@@ -1,7 +0,0 @@
-package org.GDPURJYFS.Sparrow;
-
-public class QtNativeForAndroid
-{
-     public static native void notifiedKeyboardRectangle(int x, int y,
-                                                         int width, int height);
-}
